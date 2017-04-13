eafd8da1f0e4b0a6779b57df33db50b05a0f6516
aaggarw99 <aaggarw99@gmail.com>
2017-04-13 12:43:05 -0500

    initial commit with basic log in / log out functionality with initial home screen

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,14 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+
+  protected
+
+  def configure_permitted_parameters
+
+    devise_parameter_sanitizer.permit(:sign_up, :keys => [:username, :email, :password, :location])
+    devise_parameter_sanitizer.permit(:sign_in, :keys => [:login, :password, :remember_me, :avatar_url])
+  end
+end
